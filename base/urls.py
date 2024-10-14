b90c80e3b5725636fc2446565c7bf1a6ba2ed7fe
magneticEagle <156685650+magneticEagle@users.noreply.github.com>
2024-10-14 12:57:04 -0400

    1:39:00 - CRUD

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.home, name="home"),
     path("room/<str:pk>/", views.room, name="room"),
-}
+]
